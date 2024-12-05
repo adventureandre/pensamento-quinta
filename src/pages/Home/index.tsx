90586306fdab82c11819d2d964f22e9081ee7e7c
André Luiz <adventureandre@hotmail.com>
2024-12-04 23:39:51 -0300

    style: estilizando lancamentos

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,5 @@
 import { livrosStore } from "@/store/livrosStore";
+import { Star } from "phosphor-react";
 import { useEffect } from "react";
 
 export function HomePage() {
@@ -29,12 +30,35 @@ export function HomePage() {
               <article key={livro.id} className="flex justify-center items-center flex-wrap w-[256px] mb-14">
                 <img className="w-[300px]" src={livro.imgSrc} alt={`Imagem ${livro.title}`} />
                 <h2 className="text-sm text-gray-500 text-center mx-3 w-full max-w-[256px] overflow-hidden whitespace-nowrap text-ellipsis">{livro.title}</h2>
-                <p className="text-lg mt-2 text-accent w-full text-center">{livro.price}</p>
+                <p className="text-lg mb-1 font-bold w-full text-center">{`R$ ${livro.price}`}</p>
+                <span className=" flex text-yellow-500"><Star size={21} weight="fill" /><Star size={21} weight="fill" /><Star size={21} weight="fill" /><Star size={21} weight="fill" /> </span>
               </article>
             ))
           )}
         </div>
+        <hr />
       </section>
+
+      <section className="flex flex-col items-center">
+        <h1 className="text-2xl font-semibold mt-10 px-10 mb-5">Lançamentos</h1>
+
+        <div className="flex justify-center gap-2 flex-wrap">
+          {isLoading ? (
+            <p className="w-full text-center text-xl font-semibold">Carregando...</p>
+          ) : (
+            livros?.map((livro) => (
+              <article key={livro.id} className="flex justify-center items-center flex-wrap w-[256px] mb-14">
+                <img className="w-[300px]" src="./assets/images/livro2.png" alt={`Imagem ${livro.title}`} />
+                <h2 className="text-sm text-gray-500 text-center mx-3 w-full max-w-[256px] overflow-hidden whitespace-nowrap text-ellipsis">{livro.title}</h2>
+                <p className="text-lg mb-1 font-bold w-full text-center">{`R$ ${livro.price}`}</p>
+                <span className=" flex text-yellow-500"><Star size={21} weight="fill" /><Star size={21} weight="fill" /><Star size={21} weight="fill" /><Star size={21} weight="fill" /> </span>
+              </article>
+            ))
+          )}
+        </div>
+        <hr />
+      </section>
+
     </div>
   );
 }
