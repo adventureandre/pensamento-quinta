645ed33aa372f336716ff88ce8189085888f8d99
André Luiz <adventureandre@hotmail.com>
2024-12-10 20:37:35 -0300

    fix:  link navigation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,28 @@
 import { useState } from 'react';
 import { ShoppingCart, List, UserCircle, MagnifyingGlass } from 'phosphor-react';
 import { Link } from 'react-router-dom';
-import logo  from '../../public/assets/images/logo.jpeg'
+import logo from '../../public/assets/images/logo.jpeg';
 
 export const Header = () => {
     const [isMenuOpen, setIsMenuOpen] = useState(false);
 
     return (
         <>
-            <header className="pt-6 max-w-[1600px] w-full pb-6" >
-                <div className='flex flex-row justify-between items-center  px-5 gap-10 sm:gap-1'>
+            <header className="pt-6 max-w-[1600px] w-full pb-6">
+                <div className='flex flex-row justify-between items-center px-5 gap-10 sm:gap-1'>
                     {/* Banner */}
-                    <Link className='flex flex-row gap-3 w-[500px]  items-center' to="/">
+                    <Link className='flex flex-row gap-3 w-[500px] items-center' to="/">
                         <img src={logo} alt="Pensamentos de quinta" className="w-[100px] min-w-20" />
                         <h1 className='font-playfair font-bold text-sm hidden md:block'>A Editora Que Vai Te Encantar!</h1>
                     </Link>
 
                     {/* Menu */}
-                    <nav className="hidden md:block  w-full mt-3 py-1 text-sm font-bold">
+                    <nav className="hidden md:block w-full mt-3 py-1 text-sm font-bold">
                         <ul className="flex flex-row gap-12 justify-center">
-                            <li><a className='hover:underline underline-offset-2' href="/quemsomos">Quem Somos</a></li>
-                            <li><a className='hover:underline underline-offset-2' href="#">Nossos Serviços</a></li>
-                            <li><a className='hover:underline underline-offset-2' href="#">Nossos Livros</a></li>
-                            <li><a className='hover:underline underline-offset-2' href="#">Nossos Autores</a></li>
+                            <li><Link className='hover:underline underline-offset-2' to="/quemsomos">Quem Somos</Link></li>
+                            <li><Link className='hover:underline underline-offset-2' to="#">Nossos Serviços</Link></li>
+                            <li><Link className='hover:underline underline-offset-2' to="#">Nossos Livros</Link></li>
+                            <li><Link className='hover:underline underline-offset-2' to="#">Nossos Autores</Link></li>
                         </ul>
                     </nav>
 
@@ -33,23 +33,18 @@ export const Header = () => {
                         </label>
                     </form>
 
-
-
-                    <div className="flex flex-row items-center gap-1 ">
-                        <div className='flex  gap-2 w-[180px]'>
+                    <div className="flex flex-row items-center gap-1">
+                        <div className='flex gap-2 w-[180px]'>
                             <UserCircle size={52} weight="fill" />
                             <div className='flex-col hidden lg:flex'>
                                 <span className='text-sm'>Bem-vindo(a)</span>
-                                <Link className='font-bold text-xs'
-                                    to="/dashboard/inicio">Entrar ou Cadastrar</Link>
+                                <Link className='font-bold text-xs' to="/dashboard/inicio">Entrar ou Cadastrar</Link>
                             </div>
                         </div>
                         <Link to="/checkout" aria-disabled className="relative p-2 bg-secondary rounded-md [&[aria-disabled='true']]:pointer-events-none">
                             <ShoppingCart size={22} weight="fill" />
                             <span className="w-5 h-5 rounded-full bg-accent absolute right-[-8.345px] top-[-8px] flex justify-center items-center">2</span>
                         </Link>
-
-
 
                         {/* menu mobile */}
                         <button
@@ -59,27 +54,22 @@ export const Header = () => {
                             <List size={32} />
                         </button>
                     </div>
-
                 </div>
-
-
-
-
 
                 {isMenuOpen && (
                     <nav className="absolute top-[50%] right-[50%] transform translate-x-1/2 translate-y-1/2 mt-2 bg-secondary shadow-md rounded-md md:hidden w-[250px]">
                         <ul className="flex flex-col gap-4 p-4">
-                            <li><a href="#">Home</a></li>
-                            <li><a href="#">Nossos Livros</a></li>
-                            <li><a href="#">Quem Somos</a></li>
-                            <li><a href="#">Autores</a></li>
+                            <li><Link to="/">Home</Link></li>
+                            <li><Link to="#">Nossos Livros</Link></li>
+                            <li><Link to="/quemsomos">Quem Somos</Link></li>
+                            <li><Link to="#">Autores</Link></li>
                         </ul>
                     </nav>
                 )}
             </header>
             <div className='w-full p-1' style={{
-             boxShadow: '0 4px 15px rgba(0, 0, 0, 0.1)',
-        }}></div>
+                boxShadow: '0 4px 15px rgba(0, 0, 0, 0.1)',
+            }}></div>
         </>
     );
 }
--- src/pages/DashBoard/components/menu.tsx
+++ src/pages/DashBoard/components/menu.tsx
@@ -1,22 +1,24 @@
-import logo  from '../../../../public/assets/images/logo.jpeg'
-export function MenuDashBoard(){
+import { Link } from 'react-router-dom';
+import logo from '../../../../public/assets/images/logo.jpeg';
+
+export function MenuDashBoard() {
     return (
-        <div className="w-[270px] h-[529px]  border flex justify-around flex-wrap rounded-xl"
-        style={{
-            boxShadow: '4px 4px 10px rgba(0, 0, 0, 0.1)',
-        }}
-    >
-        <div className="flex flex-row  items-center gap-3 h-[30%]">
-            <img src={logo} alt="Pensamentos de quinta" className="w-[100px] min-w-20" />
-            <span>Olá,Anna Cruz!</span>
+        <div className="w-[270px] h-[529px] border flex justify-around flex-wrap rounded-xl"
+            style={{
+                boxShadow: '4px 4px 10px rgba(0, 0, 0, 0.1)',
+            }}
+        >
+            <div className="flex flex-row items-center gap-3 h-[30%]">
+                <img src={logo} alt="Pensamentos de quinta" className="w-[100px] min-w-20" />
+                <span>Olá, Anna Cruz!</span>
+            </div>
+            <ul className="w-full h-[70%]">
+                <li className="px-4 mb-2"><Link to="inicio">Dados Pessoais</Link></li>
+                <li className="px-4 mb-2"><Link to="endereco">Endereços</Link></li>
+                <li className="px-4 mb-2"><Link to="pedidos">Pedidos</Link></li>
+                <li className="px-4 mb-2"><Link to="carteira">Carteira</Link></li>
+                <li className="px-4 mb-2"><Link to="favoritos">Favoritos</Link></li>
+            </ul>
         </div>
-        <ul className=" w-full h-[70%]">
-            <li className=" px-4 mb-2"><a href="inicio">Dados Pessoais</a></li>
-            <li className=" px-4 mb-2"><a href="endereco">Endereços</a></li>
-            <li className=" px-4 mb-2"><a href="pedidos">Pedidos</a></li>
-            <li className=" px-4 mb-2"><a href="carteira">Carteira</a></li>
-            <li className=" px-4 mb-2"><a href="favoritos">Favoritos</a></li>
-        </ul>
-    </div>
-    )
+    );
 }
