2fdc67d760a92909a72940c15fe45f1dcd2c0b52
André Luiz <adventureandre@hotmail.com>
2024-12-10 20:31:45 -0300

    fix: deploy

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,22 @@
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       colors: {
-        primary: '#E5E0D8',
-        secondary: '#B3B792',
-        accent: '#E5D2B8',
-        background: '#F5F8FA',
-        border: '#E1E8ED',
+        primary: "#E5E0D8",
+        secondary: "#B3B792",
+        accent: "#E5D2B8",
+        background: "#F5F8FA",
+        border: "#E1E8ED",
         fundoBanner: "#566150",
-        input: "#D9D9D9"
+        input: "#D9D9D9",
       },
       fontFamily: {
-        inter: ['Inter', 'sans-serif'],
-        rockSalt: ['Rock Salt', 'cursive'],
-        playfair: ['Playfair Display', 'serif']
-      }
+        inter: ["Inter", "sans-serif"],
+        rockSalt: ["Rock Salt", "cursive"],
+        playfair: ["Playfair Display", "serif"],
+      },
     },
   },
   plugins: [],
-}
+};
