76628ac71b761abd302f68217e2b20c40ec5a1db
André Luiz <adventureandre@hotmail.com>
2024-12-10 20:02:48 -0300

    fix: debug vercel

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,21 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      colors:{
+      colors: {
         primary: '#E5E0D8',
         secondary: '#B3B792',
         accent: '#E5D2B8',
         background: '#F5F8FA',
         border: '#E1E8ED',
         fundoBanner: "#566150",
-        input:"#D9D9D9"
+        input: "#D9D9D9"
       },
-      fontFamily:{
-        inter:['Inter','sans-serif'],
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
         rockSalt: ['Rock Salt', 'cursive'],
         playfair: ['Playfair Display', 'serif']
       }
