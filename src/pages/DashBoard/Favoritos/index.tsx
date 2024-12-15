3f75dce846ccbb4372d232fb827e8b5237401aeb
André Luiz <adventureandre@hotmail.com>
2024-12-15 10:08:04 -0300

    Create page Dashboard Endereço Fixes #16

--- src/pages/DashBoard/Favoritos/index.tsx
+++ src/pages/DashBoard/Favoritos/index.tsx
@@ -1,4 +1,4 @@
-import { FaHeart } from "react-icons/fa";
+import { Heart } from "phosphor-react"
 
 export function FavoritosDashboard() {
     //    interface ILivros {
@@ -34,17 +34,17 @@ export function FavoritosDashboard() {
 
     return (
         <section>
-            <h2 className="text-3xl font-semibold font-playfair">Favoritos</h2>
-            <div className="w-full h-[420px] flex flex-wrap w-full p-3 gap-x-6 mt-5 rounded-3xl">
+            <h1 className="text-3xl font-semibold font-playfair">Favoritos</h1>
+            <div className="w-full h-[420px] flex flex-wrap p-3 gap-x-6 mt-5 rounded-3xl">
                 {
                     livros.map(livro => (
                         <article className="w-52 h-[370px] flex-col justify-items-center p-5 mt-5 rounded-3xl"
                             style={{
                                 boxShadow: '4px 10px 10px rgba(0, 0, 0, 0.49)',
                             }}>
-                            <FaHeart className="justify-self-end" />
+                            <Heart className="justify-self-end" />
                             <img src={livro.src} alt={livro.titulo} className="w-30 mb-5" />
-                            <p className="font-semibold">{livro.titulo}</p>
+                            <h2 className="font-semibold">{livro.titulo}</h2>
                             <p className="font-semibold">- {livro.autor}</p>
                             <p className="font-semibold">R${livro.preço}</p>
                             <button className="bg-[#e5d2b8] hover:bg-[#deb88a] w-32 px-2 py-0.4 rounded-xl font-semibold mt-5 cursor-pointer">Comprar</button>
