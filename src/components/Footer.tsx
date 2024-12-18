44223a54f46a3e98718e22a6762627933000df68
André Luiz <adventureandre@hotmail.com>
2024-12-17 21:14:15 -0300

    fix: image footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,15 @@
 import { FacebookLogo, WhatsappLogo, InstagramLogo, TwitterLogo } from "phosphor-react";
 import { Link } from "react-router-dom";
 
+const BASE_URL = import.meta.env.VITE_BASE_URL
+
 export function Footer() {
     return (
         <footer className="bg-fundoBanner w-full flex flex-col items-center mt-5 text-white p-4">
             <div className="flex flex-col sm:flex-row justify-around mt-5 mb-5 w-full items-center">
                 <div className="flex flex-col items-center sm:items-start mb-4 sm:mb-0">
                     <Link className='flex flex-col gap-3 items-center sm:items-start' to="/">
-                        <img src="assets/images/logo.jpeg" alt="Pensamentos de quinta" className="w-[100px] min-w-20" />
+                        <img src={`${BASE_URL}/assets/images/logo.jpeg`} alt="Pensamentos de quinta" className="w-[100px] min-w-20" />
                         <h1 className='font-playfair text-white font-bold text-sm hidden md:block'>Nossa Loja Virtual.</h1>
                     </Link>
                     <div className="flex justify-center mt-2 gap-1">
