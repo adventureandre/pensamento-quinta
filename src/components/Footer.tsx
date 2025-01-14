f3eff250d8cd41160080e8c05113c549ada26ecc
veronikadelima <veronikadelima@gmail.com>
2025-01-14 13:07:15 -0300

    Início - Organização do footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,37 +1,36 @@
 import { FacebookLogo, WhatsappLogo, InstagramLogo, TwitterLogo } from "phosphor-react";
 import { Link } from "react-router-dom";
 
-const BASE_URL = import.meta.env.VITE_BASE_URL
+
+// const BASE_URL = import.meta.env.VITE_BASE_URL
+const logo = "/assets/images/logo_br.png"
 
 export function Footer() {
+
     return (
-        <footer className="bg-fundoBanner w-full flex flex-col items-center mt-5 text-white p-4">
-            <div className="flex flex-col sm:flex-row justify-around mt-5 mb-5 w-full items-center">
-                <div className="flex flex-col items-center sm:items-start mb-4 sm:mb-0">
-                    <Link className='flex flex-col gap-3 items-center sm:items-start' to="/">
-                        <img src={`${BASE_URL}/assets/images/logo.jpeg`} alt="Pensamentos de quinta" className="w-[100px] min-w-20" />
-                        <h1 className='font-playfair text-white font-bold text-sm hidden md:block'>Nossa Loja Virtual.</h1>
-                    </Link>
-                    <div className="flex justify-center mt-2 gap-1">
-                        <FacebookLogo size={32} />
-                        <WhatsappLogo size={32} />
-                        <InstagramLogo size={32} />
-                        <TwitterLogo size={32} />
+        <footer className="bg-fundoBanner w-full h-80 flex flex-col justify-center mt-5 text-white">
+            <div className="flex flex-col sm:flex-row justify-between mb-5 w-full items-center">
+                <div className="h-[160px] ml-10 w-[190px] flex flex-col items-center">
+                    <div className="flex-1 flex flex-col items-center justify-center text-center">
+                        <img src={logo} alt="Pensamentos de quinta" className="w-[100px] min-w-20 pb-1" />
+                        <h1 className="font-playfair font-bold text-sm hidden md:block pb-1 text-[#FFF]">Nossa Loja Virtual.</h1>
+                    </div>
+                    <div className="w-full h-12 flex items-center justify-around bg-[#FFF] rounded-sm">
+                        <a href="#"><FacebookLogo size={32} className="text-[#566150]"/></a>
+                        <a href="#"><WhatsappLogo size={32} className="text-[#566150]"/></a>
+                        <a href="#"><InstagramLogo size={32} className="text-[#566150]"/></a>
+                        <a href="#"><TwitterLogo size={32} className="text-[#566150]"/></a>
                     </div>
                 </div>
-                <div className="text-center sm:text-left mb-4 sm:mb-0">
-                    <h3 className="font-bold">Atendimento</h3>
-                    <p>+xx (xx) XXXXX-XXXX</p>
-                    <p>editorapensamentosdequinta@gmail.com</p>
-                    <p>Av. XXXXX XXXX, SP - 890, 00000-000 </p>
-                </div>
-                <div className="text-center sm:text-left">
-                    <h3 className="font-bold">Formas de Pagamento</h3>
-                    <img src="/assets/images/pagamentos.png" alt="Formas de Pagamento" className="mx-auto sm:mx-0"/>
+                <div className="flex flex-row pr-10">
+                    <div>
+                        
+                    </div>
+                    <div>...</div>
                 </div>
             </div>
-            <div className="text-center">
-                <p>© 2024 Editora Pensamentos de Quinta. Todos os direitos reservados. - XXXX XXXXXXXX - CNPJ XXXXXXXXXXXX</p>
+            <div className="text-center mt-5 text-[#FFF]">
+                <p className="">© 2024 Editora Pensamentos de Quinta. Todos os direitos reservados. - XXXX XXXXXXXX - CNPJ XXXXXXXXXXXX</p>
             </div>
         </footer>
     )
